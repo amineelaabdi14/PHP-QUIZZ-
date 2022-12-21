8b39ea6ebe896b311b8b413dd93638d3e7f93c7a
amineelaabdi14 <amineelaabdi@gmail.com>
2022-12-21 14:52:07 +0100

    finished the random generation of questions

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,1 +1,7 @@
-console.log(questions);
+function shuffle(){
+    for(let i=questions.length-1;i>0;i--)
+    {   
+        let randIndex= Math.floor( Math.random()*questions.length);
+        [ questions[randIndex] , questions[i] ] = [ questions[i] , questions[randIndex] ];
+    }
+}
